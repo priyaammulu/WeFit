34078e09d67e3b839c94b4750862c1f6321a138b
castogio <gioacchino.castorio@icloud.com>
2017-11-01 12:40:38 +0000

    firebase facebook login integration

--- app/src/main/java/wefit/com/wefit/LoginActivity.java
+++ app/src/main/java/wefit/com/wefit/LoginActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
@@ -25,7 +26,13 @@ import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -208,6 +215,9 @@ public class LoginActivity extends AppCompatActivity {
         @Override
         public void onSuccess(final LoginResult loginResult) {
 
+            handleFacebookAccessToken(loginResult.getAccessToken());
+
+            /*
             // request service callback handler
             GraphRequest request = GraphRequest.newMeRequest(
                     loginResult.getAccessToken(),
@@ -261,7 +271,7 @@ public class LoginActivity extends AppCompatActivity {
                                 LoginManager.getInstance().logOut();
 
                                 // go to main act
-                                startMainActivity();
+                                //startMainActivity();
 
                             }
                         }
@@ -272,6 +282,7 @@ public class LoginActivity extends AppCompatActivity {
             parameters.putString(REQUESTED_FIELDS_KEY, REQUESTED_FIELDS);
             request.setParameters(parameters);
             request.executeAsync();
+            */
 
 
         }
@@ -286,6 +297,30 @@ public class LoginActivity extends AppCompatActivity {
 
         }
 
+    }
+
+    private void handleFacebookAccessToken(AccessToken token) {
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        buildFirebaseUser(credential);
+    }
+
+    private void buildFirebaseUser(AuthCredential credential) {
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("Firebase success", "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            // TODO cosa fare ora?
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("FAIL", "signInWithCredential:failure", task.getException());
+                        }
+
+                    }
+                });
     }
 
     /**
